526456b3a792a89617a08c61e020c234a5783d76
Jamie Nuttall <jamie@wattu.com>
2024-11-17 20:11:05 +0000

    removed topic from params

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
@@ -52,19 +53,49 @@ func main() {
 		port = os.Getenv("PORT")
 	}
 
-	topic := "worklog"
-
-	if os.Getenv("EVENT_TOPIC") != "" {
-		topic = os.Getenv("EVENT_TOPIC")
-	}
-
 	if os.Getenv("EVENT_STORE") == "" || os.Getenv("EVENT_STREAM") == "" {
 		workService = services.NewWorkService(ctx, repo.NewWorkLogRepository())
 	} else {
-		t := common.NewHttpTransport(os.Getenv("EVENT_STORE"), os.Getenv("EVENT_STREAM"), 10)
-		eventBroadcaster := common.NewEventService(repo.NewWorkLogRepository(), t, topic)
-		workService = services.NewWorkService(ctx, eventBroadcaster)
+		t := common.NewHttpTransport(os.Getenv("EVENT_STORE"), os.Getenv("EVENT_STREAM"), 0)
 
+		es := common.NewEventService(repo.NewWorkLogRepository(), t, "WorkLog")
+
+		workService = services.NewWorkService(ctx, es)
+
+		//es.StartEventRunner(ctx)
+
+		go func() {
+
+			slog.Info("Starting event runner")
+
+			err := es.Connect(ctx)
+
+			if err != nil {
+				slog.Error("Error connecting to event store", "error", err)
+				return
+			}
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					slog.Info("Waiting for event")
+					ev, err := es.NextEvent()
+
+					slog.Info("Got event", "event", ev, "error", err)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					if ev != nil {
+						slog.Info("Handling event", "event", *ev)
+						es.HandleEvent(*ev)
+					} else {
+						slog.Info("No event")
+					}
+				}
+			}
+		}()
 	}
 
 	if err := startWebServer(port, workService); err != nil {
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/papawattu/cleanlog-worklog
 
 go 1.23.1
 
-require (
-	github.com/papawattu/cleanlog-common v0.0.4
-)
+require github.com/papawattu/cleanlog-common v0.0.11
